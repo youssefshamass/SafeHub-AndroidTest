7d7d4c31e791e54c26cefdbdd5c5eef2d68d929e
Youssef Shamass <my.shamass@gmail.com>
2021-06-09 20:37:11 +0300

    Prepare for viewmodels testing

---
+++ app/src/test/java/com/youssefshamass/safehub/presentation/home/HomeViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.youssefshamass.safehub.presentation.home
+
+import com.nhaarman.mockitokotlin2.mock
+import com.youssefshamass.data.entities.local.User
+import com.youssefshamass.domain.users.ObservePreviousMatches
+import com.youssefshamass.domain.users.SearchUser
+import junit.framework.TestCase
+import org.junit.After
+import org.junit.Before
+
+class HomeViewModelTest {
+
+    private val observePreviousMatchesUseCase = mock<ObservePreviousMatches>()
+    private val searchUserUseCase = mock<SearchUser>()
+
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var user: User
+
+    @Before
+    fun setUp() {
+        viewModel = HomeViewModel(observePreviousMatchesUseCase, searchUserUseCase)
+        user = User(
+            1,
+            "youssefshamass",
+            "http://example.com",
+            "Youssef shamass",
+            "-",
+            "Damascus - Syria",
+            "Bio",
+            0,
+            0
+        )
+    }
+
+
+}
